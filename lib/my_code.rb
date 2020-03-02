ffed96f08548e91e2c6cecea0d86f6635de37f89
Taiye Salami <taiye41@gmail.com>
2020-03-02 13:09:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -19,6 +19,10 @@ def reduce_to_total(source_array, starting_point)
   source_array.reduce(starting_point, :+)
 end
 
+def reduce_to_total(source_array)
+  source_array.reduce(:+)
+end
+
 def reduce_to_all_true(source_array)
   source_array.all?
 end
