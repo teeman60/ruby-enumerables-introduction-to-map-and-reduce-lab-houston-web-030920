090be66da692cdcd7d8c693db9794367dce38de1
Taiye Salami <taiye41@gmail.com>
2020-03-02 13:21:54 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,9 +15,7 @@ def map_to_square(source_array)
   source_array.map {|num| num ** 2}
 end
 
-def reduce_to_total(source_array)
-  source_array.reduce(:+)
-end
+
 
 def reduce_to_total(source_array, starting_point)
   source_array.reduce(starting_point, :+)
